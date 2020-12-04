4b91cff3579d77cc6e7b78e8703f724db452c4b6
Tim Babych <tim.babych@gmail.com>
2020-12-03 18:32:16 -0800

    reformat comment

--- 3/sled.go
+++ 3/sled.go
@@ -35,7 +35,8 @@ Count the number of trees encounted in such diagonal manner.
 
 Part 2:
 
-Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+Determine the number of trees you would encounter if, for each of the following slopes,
+you start at the top-left corner and traverse the map all the way to the bottom:
 
     Right 1, down 1.
     Right 3, down 1. (This is the slope you already checked.)
