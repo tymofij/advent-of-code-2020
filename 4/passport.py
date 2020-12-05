1e5e819545e775ab3dfd22ae3505558026a997b4
Tim Babych <tim.babych@gmail.com>
2020-12-04 20:14:50 -0800

    day 4, part 2, python

--- 4/passport.py
+++ 4/passport.py
@@ -1,19 +1,69 @@
+import re
+
+def is_valid_birth_year(s):
+  try:
+    year = int(s)
+  except:
+    return False
+  return 1920 <=year <= 2002
+
+def is_valid_issue_year(s):
+  try:
+    year = int(s)
+  except:
+    return False
+  return 2010 <=year <= 2020
+
+def is_valid_exp_year(s):
+  try:
+    year = int(s)
+  except:
+    return False
+  return 2020 <=year <= 2030
+
+def is_valid_height(s):
+  n, unit = s[:-2], s[-2:]
+  try:
+    n = int(n)
+  except:
+    return False
+  if unit == 'cm':
+    return 150 <= n <= 193
+  if unit == 'in':
+    return 59 <= n <= 76
+
+def is_valid_hair_color(s):
+  return re.match(r'^#[\da-f]{6}$', s)
+
+def is_valid_eye_color(s):
+  return s in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+
+def is_valid_pid(s):
+  return re.match(r'^\d{9}$', s)
+
+
 REQUIRED_FIELDS = {
-		"byr", # Birth Year
-		"iyr", # Issue Year
-		"eyr", # Expiration Year
-		"hgt", # Height
-		"hcl", # Hair Color
-		"ecl", # Eye Color
-		"pid", # Passport ID
-		# "cid", # Country ID
+    "byr": is_valid_birth_year , # Birth Year
+    "iyr": is_valid_issue_year,  # Issue Year
+    "eyr": is_valid_exp_year,    # Expiration Year
+    "hgt": is_valid_height,      # Height
+    "hcl": is_valid_hair_color,  # Hair Color
+    "ecl": is_valid_eye_color,   # Eye Color
+    "pid": is_valid_pid,         # Passport ID
+    # "cid", # Country ID
 }
 
 passports = [chunk.split() for chunk in open("input.txt").read().split("\n\n")]
 
-def field_names(passport_data):
-    return set(line.split(':')[0] for line in passport_data)
+def is_valid_passport(passport):
+  data = dict(line.split(':')  for line in passport)
+  for field, func in REQUIRED_FIELDS.items():
+    if field not in data:
+      return False
+    if not func(data[field]):
+      return False
+  return True
 
 print(len(
-    [True for p in passports if not REQUIRED_FIELDS - field_names(p)]
+    [True for p in passports if is_valid_passport(p)]
 ))
