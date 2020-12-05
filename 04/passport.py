7d00aecb1d3547095a0d172432a768a234016691
Tim Babych <tim.babych@gmail.com>
2020-12-05 11:26:47 -0800

    lambdas

--- 04/passport.py
+++ 04/passport.py
@@ -1,55 +1,22 @@
 import re
 
-def is_valid_birth_year(s):
-  try:
-    year = int(s)
-  except:
-    return False
-  return 1920 <=year <= 2002
-
-def is_valid_issue_year(s):
-  try:
-    year = int(s)
-  except:
-    return False
-  return 2010 <=year <= 2020
-
-def is_valid_exp_year(s):
-  try:
-    year = int(s)
-  except:
-    return False
-  return 2020 <=year <= 2030
 
 def is_valid_height(s):
-  n, unit = s[:-2], s[-2:]
-  try:
-    n = int(n)
-  except:
-    return False
+  n, unit = int(s[:-2]), s[-2:]
   if unit == 'cm':
     return 150 <= n <= 193
   if unit == 'in':
     return 59 <= n <= 76
 
-def is_valid_hair_color(s):
-  return re.match(r'^#[\da-f]{6}$', s)
-
-def is_valid_eye_color(s):
-  return s in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-
-def is_valid_pid(s):
-  return re.match(r'^\d{9}$', s)
-
 
 REQUIRED_FIELDS = {
-    "byr": is_valid_birth_year , # Birth Year
-    "iyr": is_valid_issue_year,  # Issue Year
-    "eyr": is_valid_exp_year,    # Expiration Year
-    "hgt": is_valid_height,      # Height
-    "hcl": is_valid_hair_color,  # Hair Color
-    "ecl": is_valid_eye_color,   # Eye Color
-    "pid": is_valid_pid,         # Passport ID
+    "byr": lambda s: 1920 <=int(s) <= 2002, # Birth Year
+    "iyr": lambda s: 2010 <=int(s) <= 2020, # Issue Year
+    "eyr": lambda s: 2020 <=int(s) <= 2030, # Expiration Year
+    "hgt": is_valid_height, # Height
+    "hcl": lambda s: re.match(r'^#[\da-f]{6}$', s), # Hair Color
+    "ecl": lambda s: s in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}, # Eye Color
+    "pid": lambda s:re.match(r'^\d{9}$', s), # Passport ID
     # "cid", # Country ID
 }
 
@@ -60,7 +27,10 @@ def is_valid_passport(passport):
   for field, func in REQUIRED_FIELDS.items():
     if field not in data:
       return False
-    if not func(data[field]):
+    try:
+      if not func(data[field]):
+        return False
+    except:
       return False
   return True
 
