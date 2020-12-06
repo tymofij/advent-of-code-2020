71e09cd89810a3a59936963e73cced9a0666f6e7
Tim Babych <tim.babych@gmail.com>
2020-12-05 20:05:55 -0800

    no max

--- 05/seats.py
+++ 05/seats.py
@@ -3,7 +3,7 @@ def get_seat_id(s):
               .replace('L','0').replace('R','1'), 2)
 
 seat_ids = sorted(get_seat_id(s) for s in open('input.txt').readlines())
-print('Max:', max(seat_ids))
+print('Max:', seat_ids[-1])
 
 for i, v in enumerate(seat_ids):
   if i > 0 and seat_ids[i-1]+1 != v:
