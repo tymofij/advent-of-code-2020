224205e80a62b0d649fb4cf935e81981ed9eb648
Tim Babych <tim.babych@gmail.com>
2020-12-07 22:23:38 -0800

    use slice pointers

--- 08/loops.go
+++ 08/loops.go
@@ -14,18 +14,16 @@ type instruction struct {
 
 const ACC, NOP, JMP = "acc", "nop", "jmp"
 
-var code = []instruction{}
-
-func run(code []instruction) (acc int, looped bool) {
+func run(code *[]instruction) (acc int, looped bool) {
 	i := 0
 	visited := map[int]bool{}
-	for i < len(code) {
+	for i < len(*code) {
 		_, ok := visited[i]
 		if ok {
 			return acc, true
 		}
 		visited[i] = true
-		instr := code[i]
+		instr := (*code)[i]
 		switch instr.op {
 		case ACC:
 			acc += instr.n
@@ -42,13 +40,14 @@ func run(code []instruction) (acc int, looped bool) {
 func main() {
 	data, _ := ioutil.ReadFile("input.txt")
 	text := strings.Split(strings.TrimSpace(string(data)), "\n")
+	code := make([]instruction, 0, 700)
 	for _, line := range text {
 		line := strings.Fields(line)
 		n, _ := strconv.Atoi(line[1])
 		code = append(code, instruction{line[0], n})
 	}
 
-	acc, _ := run(code)
+	acc, _ := run(&code)
 	fmt.Println("Accumulator at first loop:", acc)
 
 	for i := 0; i < len(code); i++ {
@@ -61,7 +60,7 @@ func main() {
 		} else {
 			code[i].op = NOP
 		}
-		acc, looped := run(code)
+		acc, looped := run(&code)
 		if !looped {
 			fmt.Println("Final accumulator in fixed code:", acc)
 			break
