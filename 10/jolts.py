264c6e049bde7685ea35261713fd6d3d0e0ef5d7
Tim Babych <tim.babych@gmail.com>
2020-12-09 22:34:16 -0800

    Python jolts with memoization

--- 10/jolts.py
+++ 10/jolts.py
@@ -1,20 +1,23 @@
-numbers = [int(s) for s in  open('i.txt').readlines()]
+numbers = [int(s) for s in open('input.txt').readlines()]
 numbers.append(0)
 numbers.sort()
 
+cache = {}
+
 def count_ways(nums):
+    last = nums[-1]
+    if last in cache:
+        return cache[last]
     res = 0
     if len(nums) <= 2:
         return 1
-    if nums[-1] - nums[-2] <= 3:
+    if last - nums[-2] <= 3:
         res += count_ways(nums[:-1])
-    try:
-        if nums[-1] - nums[-3] <= 3:
-            res += count_ways(nums[:-2])
-        if nums[-1] - nums[-4] <= 3:
-            res += count_ways(nums[:-3])
-    except IndexError:
-        pass
+    if len(nums) >=3 and last - nums[-3] <= 3:
+        res += count_ways(nums[:-2])
+    if len(nums) >=4 and last - nums[-4] <= 3:
+        res += count_ways(nums[:-3])
+    cache[last] = res
     return res
 
 print(count_ways(numbers))
