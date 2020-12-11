b7eecd1b34f331c009304b38dd3311130c550b01
Tim Babych <tim.babych@gmail.com>
2020-12-11 02:06:10 -0800

    tests

--- 11/seats_test.go
+++ 11/seats_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 )
 
-func getData() [][]byte {
-	data, _ := ioutil.ReadFile("input.txt")
+func getData(name string) [][]byte {
+	data, _ := ioutil.ReadFile(name)
 	text := strings.TrimSpace(string(data))
 	seats := [][]byte{}
 	for _, line := range strings.Split(text, "\n") {
@@ -16,8 +16,40 @@ func getData() [][]byte {
 	return seats
 }
 
+func TestNearby(t *testing.T) {
+	seats := getData("i.txt")
+	got := occupiedSeatsWhenStabilized(seats, 4, 1)
+	if got != 37 {
+		t.Errorf("got %d for occupiedSeatsWhenStabilized", got)
+	}
+}
+
+func TestNearbyNoalloc(t *testing.T) {
+	seats := getData("i.txt")
+	got := occupiedSeatsWhenStabilizedNoalloc(seats, 4, 1)
+	if got != 37 {
+		t.Errorf("got %d for occupiedSeatsWhenStabilizedNoalloc", got)
+	}
+}
+
+func TestVisible(t *testing.T) {
+	seats := getData("i.txt")
+	got := occupiedSeatsWhenStabilized(seats, 5, -1)
+	if got != 26 {
+		t.Errorf("got %d for occupiedSeatsWhenStabilized", got)
+	}
+}
+
+func TestVisibleNoalloc(t *testing.T) {
+	seats := getData("i.txt")
+	got := occupiedSeatsWhenStabilizedNoalloc(seats, 5, -1)
+	if got != 26 {
+		t.Errorf("got %d for occupiedSeatsWhenStabilizedNoalloc", got)
+	}
+}
+
 func BenchmarkOccupied(b *testing.B) {
-	seats := getData()
+	seats := getData("input.txt")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		occupiedSeatsWhenStabilized(seats, 5, -1)
@@ -25,7 +57,7 @@ func BenchmarkOccupied(b *testing.B) {
 }
 
 func BenchmarkOccupiedNoalloc(b *testing.B) {
-	seats := getData()
+	seats := getData("input.txt")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		occupiedSeatsWhenStabilizedNoalloc(seats, 5, -1)
