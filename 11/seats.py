40cace97cb3cb947e9aada29be9154b8af83544e
Tim Babych <tim.babych@gmail.com>
2020-12-10 22:00:04 -0800

    day 11, Py, part 2

--- 11/seats.py
+++ 11/seats.py
@@ -6,7 +6,7 @@ EMPTY = 'L'
 TAKEN = '#'
 
 
-def count_taken_around(seats, i, j):
+def count_taken_nearby(seats, i, j):
     res = 0
     for i_step in (-1, 0, 1):
         for j_step in (-1, 0, 1):
@@ -19,29 +19,66 @@ def count_taken_around(seats, i, j):
     return res
 
 
-def next_state(seats):
+def count_visible(seats, i, j):
+    res = 0
+    directions = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0,  -1),          (0,  1),
+        (1,  -1), (1,  0), (1 , 1)
+    ]
+    for (di, dj) in directions:
+        ni, nj = i+di, j+dj
+        while ((0 <= ni <= len(seats)-1) and
+               (0 <= nj <= len(seats[0])-1)):
+            if seats[ni][nj] == EMPTY:
+                break
+            if seats[ni][nj] == TAKEN:
+                res += 1
+                break
+            ni, nj = ni+di, nj+dj
+    return res
+
+
+def next_state_1(seats):
     new = deepcopy(seats)
     for i in range(len(seats)):
         for j in range(len(seats[0])):
             if seats[i][j] == FLOOR:
                 continue
-            neighbours = count_taken_around(seats, i, j)
+            neighbours = count_taken_nearby(seats, i, j)
             if seats[i][j] == EMPTY and neighbours == 0:
                 new[i][j] = TAKEN
             if seats[i][j] == TAKEN and neighbours >= 4:
                 new[i][j] = EMPTY
     return new
 
+
+def next_state_2(seats):
+    new = deepcopy(seats)
+    for i in range(len(seats)):
+        for j in range(len(seats[0])):
+            if seats[i][j] == FLOOR:
+                continue
+            neighbours = count_visible(seats, i, j)
+            if seats[i][j] == EMPTY and neighbours == 0:
+                new[i][j] = TAKEN
+            if seats[i][j] == TAKEN and neighbours >= 5:
+                new[i][j] = EMPTY
+    return new
+
+
 def show(seats):
     for line in seats:
         print(''.join(line))
     print()
 
+
 prev_seats = deepcopy(seats)
-seats = next_state(seats)
+seats = next_state_2(seats)
 while seats != prev_seats:
+    # show(seats)
     prev_seats = deepcopy(seats)
-    seats = next_state(seats)
+    seats = next_state_2(seats)
 
 res = 0
 for line in seats:
