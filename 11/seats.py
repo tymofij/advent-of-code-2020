abe68055d2a3a186ee4912f2ed7f83df104a6098
Tim Babych <tim.babych@gmail.com>
2020-12-10 22:21:55 -0800

    refactor to combine parts

--- 11/seats.py
+++ 11/seats.py
@@ -5,21 +5,7 @@ FLOOR = '.'
 EMPTY = 'L'
 TAKEN = '#'
 
-
-def count_taken_nearby(seats, i, j):
-    res = 0
-    for i_step in (-1, 0, 1):
-        for j_step in (-1, 0, 1):
-            if (0 <= i+i_step <= len(seats)-1 and
-               0 <= j+j_step <= len(seats[0])-1 and
-               not (i_step==0 and j_step==0) and
-               seats[i+i_step][j+j_step] == TAKEN
-               ):
-                res += 1
-    return res
-
-
-def count_visible(seats, i, j):
+def count_visible(seats, i, j, visibility_limit=None):
     res = 0
     directions = [
         (-1, -1), (-1, 0), (-1, 1),
@@ -28,6 +14,7 @@ def count_visible(seats, i, j):
     ]
     for (di, dj) in directions:
         ni, nj = i+di, j+dj
+        steps = 1
         while ((0 <= ni <= len(seats)-1) and
                (0 <= nj <= len(seats[0])-1)):
             if seats[ni][nj] == EMPTY:
@@ -35,34 +22,23 @@ def count_visible(seats, i, j):
             if seats[ni][nj] == TAKEN:
                 res += 1
                 break
+            if visibility_limit and steps >= visibility_limit:
+                break
             ni, nj = ni+di, nj+dj
+            steps += 1
     return res
 
 
-def next_state_1(seats):
+def next_state(seats, tolerance, visibility_limit=None):
     new = deepcopy(seats)
     for i in range(len(seats)):
         for j in range(len(seats[0])):
             if seats[i][j] == FLOOR:
                 continue
-            neighbours = count_taken_nearby(seats, i, j)
+            neighbours = count_visible(seats, i, j, visibility_limit)
             if seats[i][j] == EMPTY and neighbours == 0:
                 new[i][j] = TAKEN
-            if seats[i][j] == TAKEN and neighbours >= 4:
-                new[i][j] = EMPTY
-    return new
-
-
-def next_state_2(seats):
-    new = deepcopy(seats)
-    for i in range(len(seats)):
-        for j in range(len(seats[0])):
-            if seats[i][j] == FLOOR:
-                continue
-            neighbours = count_visible(seats, i, j)
-            if seats[i][j] == EMPTY and neighbours == 0:
-                new[i][j] = TAKEN
-            if seats[i][j] == TAKEN and neighbours >= 5:
+            if seats[i][j] == TAKEN and neighbours >= tolerance:
                 new[i][j] = EMPTY
     return new
 
@@ -73,16 +49,20 @@ def show(seats):
     print()
 
 
-prev_seats = deepcopy(seats)
-seats = next_state_2(seats)
-while seats != prev_seats:
-    # show(seats)
-    prev_seats = deepcopy(seats)
-    seats = next_state_2(seats)
+def occupied_seats_when_stabilized(seats, tolerance, visibility_limit=None):
+    prev_seats = seats
+    seats = next_state(seats, tolerance, visibility_limit)
+    while seats != prev_seats:
+        # show(seats)
+        prev_seats = seats
+        seats = next_state(seats, tolerance, visibility_limit)
 
-res = 0
-for line in seats:
-    for seat in line:
-        if seat == TAKEN:
-            res += 1
-print(res)
+    res = 0
+    for line in seats:
+        for seat in line:
+            if seat == TAKEN:
+                res += 1
+    return res
+
+print("part 1", occupied_seats_when_stabilized(seats, 4, 1))
+print("part 2", occupied_seats_when_stabilized(seats, 5))
