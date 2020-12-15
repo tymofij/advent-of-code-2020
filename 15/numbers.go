17a35177f3f879977829b3f36bd68e046bc9a6cd
Tim Babych <tim.babych@gmail.com>
2020-12-14 21:57:33 -0800

    day 15, Golang

---
+++ 15/numbers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	data := []int{10, 16, 6, 0, 1, 17}
+	lastSpoken := map[int]int{}
+	for k, v := range data {
+		if k < len(data)-1 {
+			lastSpoken[v] = k + 1
+		}
+	}
+	prev := data[len(data)-1]
+	turn := len(data)
+
+	var next int
+	for turn < 30000000 {
+		prevTurn, ok := lastSpoken[prev]
+		if ok {
+			next = turn - prevTurn
+		} else {
+			next = 0
+		}
+		lastSpoken[prev] = turn
+		prev = next
+		turn++
+	}
+
+	fmt.Println(turn, prev)
+}
--- 15/numbers.py
+++ 15/numbers.py
@@ -4,7 +4,7 @@ last_spoken = {v: k+1 for k, v in enumerate(data[:-1])}
 prev = data[-1]
 turn = len(data)
 
-while turn < 2020:
+while turn < 30000000:
     new = turn - last_spoken[prev] if prev in last_spoken else 0
     last_spoken[prev] = turn
     prev = new
