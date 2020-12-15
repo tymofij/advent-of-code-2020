85a83e32e82feddad747881ed1d58a434a28a9fb
Tim Babych <tim.babych@gmail.com>
2020-12-14 21:27:54 -0800

    numbers, py 2

--- 15/numbers.py
+++ 15/numbers.py
@@ -1,13 +1,11 @@
-# data = (0,3,6)
-data = 10,16,6,0,1,17
+data = (10,16,6,0,1,17)
 
 last_spoken = {v: k+1 for k, v in enumerate(data[:-1])}
 
 prev = data[-1]
 turn = len(data)
 
-while turn <= 2020:
-    print(turn, prev)
+while turn < 30000000:
     if prev not in last_spoken:
         new = 0
     else:
@@ -17,3 +15,5 @@ while turn <= 2020:
 
     prev = new
     turn += 1
+
+print(turn, prev)
