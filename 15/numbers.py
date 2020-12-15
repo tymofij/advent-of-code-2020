0adab092da4911469b70cf75b76bbc6617b6e68e
Tim Babych <tim.babych@gmail.com>
2020-12-14 21:34:14 -0800

    ternary

--- 15/numbers.py
+++ 15/numbers.py
@@ -1,18 +1,12 @@
 data = (10,16,6,0,1,17)
 
 last_spoken = {v: k+1 for k, v in enumerate(data[:-1])}
-
 prev = data[-1]
 turn = len(data)
 
-while turn < 30000000:
-    if prev not in last_spoken:
-        new = 0
-    else:
-        new = turn - last_spoken[prev]
-
+while turn < 2020:
+    new = turn - last_spoken[prev] if prev in last_spoken else 0
     last_spoken[prev] = turn
-
     prev = new
     turn += 1
 
