13da8721723d203eddfceb7b104e6983fc082dee
Tim Babych <tim.babych@gmail.com>
2020-12-20 02:23:37 -0800

    day 20, demo

--- 20/puzzle.py
+++ 20/puzzle.py
@@ -4,6 +4,10 @@ import math, sys
 VARIATIONS = 12
 
 def to_struct(top, right, bottom, left):
+    assert top[0] == left[0]
+    assert top[-1] == right[0]
+    assert bottom[0] == left[-1]
+    assert bottom[-1] == right[-1]
     return {
         'top': top,
         'left': left,
@@ -11,11 +15,14 @@ def to_struct(top, right, bottom, left):
         'bottom': bottom,
     }
 
-def rotations(a, b, c, d):
-    yield a, b, c, d
-    yield b, c, d, a
-    yield c, d, a, b
-    yield d, a, b, c
+def rotations(top, right, bottom, left):
+    yield top, right, bottom, left
+    top, right, bottom, left = right, bottom[::-1], left, top[::-1]
+    yield top, right, bottom, left
+    top, right, bottom, left = right, bottom[::-1], left, top[::-1]
+    yield top, right, bottom, left
+    top, right, bottom, left = right, bottom[::-1], left, top[::-1]
+    yield top, right, bottom, left
 
 def flips(top, right, bottom, left):
     yield from rotations(top, right, bottom, left) # norm
@@ -66,68 +73,46 @@ chain = [{
     'v': None, # - to be filled in
 }]
 
-# m = 0
-# while chain:
-#     print(len(chain))
-#     print('chain', chain)
-#     m = max(m, len(chain))
-#     if len(chain) == SIZE*SIZE:
-#         break # we're done
-#     i = len(chain)  # next elem to assign
-#     cur = chain[i-1]
-#     if not cur['options']:
-#         print('out of options, taking step back')
-#         chain.pop() # step back
-#         continue
-#     n, v = cur['options'].pop()
-#     cur['n'] = n
-#     cur['v'] = v
-#     cur['exclude'] = {n} if i == 1 else chain[i-2]['exclude'] | {n}
-#     print('evaluating', cur)
-#     left = chain[i-1] if i % SIZE != 0 else None
-#     top = chain[i-SIZE] if i >= SIZE else None
-#     next_ones = stitch(get_tile(left), get_tile(top), cur['exclude'])
-#     if next_ones:
-#         print('got options, extending')
-#         chain.append({
-#             'options': next_ones,
-#             'exclude': None,
-#             'n': None, # - to be filled in
-#             'v': None, # - to be filled in
-#         })
-#     else:
-#         print('could not find next tile')
+DEBUG = 0
+def log(*args):
+    if DEBUG:
+        print(*args)
+
+m = 0
+while chain:
+    log(len(chain))
+    log('chain', chain)
+    m = max(m, len(chain))
+    i = len(chain)  # next elem to assign
+    cur = chain[i-1]
+    if not cur['options']:
+        log('out of options, taking step back')
+        chain.pop() # step back
+        continue
+    n, v = cur['options'].pop()
+    cur['n'] = n
+    cur['v'] = v
+    cur['exclude'] = {n} if i == 1 else chain[i-2]['exclude'] | {n}
+    if len(chain) == SIZE*SIZE:
+        break # we're done
+    log('evaluating', cur)
+    left = chain[i-1] if i % SIZE != 0 else None
+    top = chain[i-SIZE] if i >= SIZE else None
+    next_ones = stitch(get_tile(left), get_tile(top), cur['exclude'])
+    if next_ones:
+        log('got options, extending')
+        chain.append({
+            'options': next_ones,
+            'exclude': None,
+            'n': None, # - to be filled in
+            'v': None, # - to be filled in
+        })
+    else:
+        log('could not find next tile')
 
 # print('------')
 # print(m, len(chain))
 # for link in chain:
 #     print(link['n'], link['v'], link['exclude'])
 
-sol = []
-for triplet in open('sol.txt').read().split('\n\n'):
-    one, two, three = [], [], []
-    for line in triplet.split('\n'):
-        a, b, c = line.split()
-        one.append(a)
-        two.append(b)
-        three.append(c)
-    sol.extend([one, two, three])
-
-for data in sol:
-    top = data[0]
-    bottom = data[-1]
-    left = ''.join([line[0] for line in data])
-    right = ''.join([line[-1] for line in data])
-    found = False
-    for n in tiles.keys():
-        for v in range(VARIATIONS):
-            t = tiles[n][v]
-            if t['left'] == left and t['right'] == right and t['top'] == top and t['bottom'] == bottom:
-                print('FOUND!')
-                found = True
-    if not found:
-        for line in data:
-            print(line)
-
-
-
+print('!!', chain[0]['n'] * chain[-1]['n'] * chain[SIZE-1]['n'] * chain[SIZE*(SIZE-1)]['n'])
