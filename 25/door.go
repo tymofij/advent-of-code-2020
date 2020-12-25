dcc9343c0f928e2c67a5597c9a0f88000eab0719
Tim Babych <tim.babych@gmail.com>
2020-12-24 21:28:44 -0800

    day 25, part 1

---
+++ 25/door.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func transform(subject int, loops int) int {
+	res := 1
+	i := 1
+	for i = 1; i <= loops; i++ {
+		res *= subject
+		res %= 20201227
+	}
+	fmt.Println(i, res)
+	return 0
+}
+
+// loop 7177897  == pubkey 19072108
+// loop 7779516  == pubkey 1965712
+// encryption key == 16881444
+
+func main() {
+	transform(1965712, 7177897)
+	transform(19072108, 7779516)
+}
