ec1a796d12b6fa5675db82d9966555677c5524cf
Tim Babych <tim.babych@gmail.com>
2021-11-03 01:41:30 -0700

    list

--- 18/a/calc_test.go
+++ 18/a/calc_test.go
@@ -4,21 +4,45 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	input    string
+	expected int
+}
+
 func TestSimpleCalc(t *testing.T) {
+	cases := []testCase{
+		{
+			input:    "2 * 3",
+			expected: 6,
+		},
+		{
+			input:    "1 + 2 * 3 + 4 * 5 + 6",
+			expected: 71,
+		},
+		{
+			input:    "2 * 3 + (4 * 5)",
+			expected: 26,
+		},
+		{
+			input:    "5 + (8 * 3 + 9 + 3 * 4 * 3)",
+			expected: 437,
+		},
+		{
+			input:    "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+			expected: 12240,
+		},
+		{
+			input:    "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",
+			expected: 13632,
+		},
+	}
+
 	var result int
-
-	result = SimpleCalc("2 * 3")
-	if result != 6 {
-		t.Errorf("Got %d, expected %d", result, 6)
+	for _, c := range cases {
+		result = SimpleCalc(c.input)
+		if result != c.expected {
+			t.Errorf("Got %d, expected %d", result, c.expected)
+		}
 	}
 
-	result = SimpleCalc("1 + 2 * 3 + 4 * 5 + 6")
-	if result != 71 {
-		t.Errorf("Got %d, expected %d", result, 71)
-	}
-
-	// "2 * 3 + (4 * 5)" == 26
-	// "5 + (8 * 3 + 9 + 3 * 4 * 3)" == 437
-	// "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" == 12240
-	// "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" == 13632
 }
